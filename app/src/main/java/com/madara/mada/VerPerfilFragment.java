7b4a57c0d857e7b5bdcff0dc80e12734fc321d62
ChuyJimenez <44859178+ChuyJimenez@users.noreply.github.com>
2018-12-06 02:00:10 -0600

    Ya puse diseño a la parte de Actualizar al usuario

--- app/src/main/java/com/madara/mada/VerPerfilFragment.java
+++ app/src/main/java/com/madara/mada/VerPerfilFragment.java
@@ -34,14 +34,7 @@ import java.io.File;
 import java.util.ArrayList;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link VerPerfilFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link VerPerfilFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class VerPerfilFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
